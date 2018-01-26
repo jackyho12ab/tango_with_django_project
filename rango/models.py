db72a790eb46e71e7e1d87607bc53ddf914d456f
Chak Lam Ho <2268750h@student.gla.ac.uk>
2018-01-26 12:28:07 +0900

    first commit

---
+++ rango/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self): 
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
