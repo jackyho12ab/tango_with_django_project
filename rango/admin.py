977e1e467f3aa591f978749db368f8ae0121b758
Chak Lam Ho <2268750h@student.gla.ac.uk>
2018-02-08 11:18:38 +0900

    first commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
-
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
@@ -12,5 +13,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
-
+admin.site.register(UserProfile)
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -110,8 +110,20 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
+    },
 ]
 
+PASSWORD_HASHERS = (
+    
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
